f92e97f0b5b9357c858ff27d3812e680ae24c422
sandesh2057 <sandeshbhattarai6@gmail.com>
2025-06-14 20:18:58 +0545

    initial change for test

---
+++ racingTurtles.py
@@ -0,0 +1,6 @@
+def get_number_of_racers():
+    racers = 0
+    while True:
+        racers = input("Enter the number of racers (2-10): ")
+        if racers.isdigit():
+            racers = int(racers)
